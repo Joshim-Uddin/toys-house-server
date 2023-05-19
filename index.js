8f3af503c869bda3b944adbaf7509e53bbf2882e
Joshim Uddin <mju881@gmail.com>
2023-05-19 11:44:02 +0600

    data by search implemented

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ app.use(cors());
 app.use(express.json())
 
 app.get('/',(req, res)=>{
-    res.send('Server is runnging')
+    res.send('Server is running')
 })
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x5isz8r.mongodb.net/?retryWrites=true&w=majority`;
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,10 +26,47 @@ const client = new MongoClient(uri, {
   }
 });
 
+const toysCollections = client.db('toysData').collection('toys');
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.get('/alltoys', async (req, res) => {
+        const result = await toysCollections.find().limit(20).toArray();
+        res.send(result);
+        
+    })
+    app.get('/alltoys/:text', async (req, res) => {
+       const text = req.params.text;
+       if(text == 'Regular Car' || text == 'Sports Car' || text == 'Truck'){
+        const result = await toysCollections.find({subCategory:text}).toArray();
+        res.send(result);
+       }else{
+        const result = await toysCollections.find().toArray();
+        res.send(result);
+       }
+        
+    })
+
+    const result = await toysCollections.createIndex({name: 1})
+
+    app.get('/allcategorytoys/:text', async (req, res) => {
+      const searchText = req.params.text;
+        const result = await toysCollections.find({name:{$regex: searchText, $options:'i'}}).toArray();
+          res.send(result);
+    })
+    app.get('/allcategorytoys/', async (req, res) => {
+        const result = await toysCollections.find().toArray();
+          res.send(result);
+    })
+
+    app.post('/addtoy', async (req, res) => {
+        const body = req.body;
+        const result = await toysCollections.insertOne(body)
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
